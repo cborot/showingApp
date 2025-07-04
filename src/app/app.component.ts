e8ef918de2f4393e93498700dc156a7064229027
Cynthia Parchoux Borot <cynthia.borot@ecomail.fr>
2025-07-04 16:01:23 +0200

    Before @angular/common 19.2.0 > 20.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from "@ng-doc/app";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
